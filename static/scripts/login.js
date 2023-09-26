bc6c34a7a7799c16fb458d6f373a7854580e69d9
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-09-25 23:30:15 -0300

    add: login structure

---
+++ static/scripts/login.js
@@ -0,0 +1,58 @@
+// Função para registrar um novo usuario e gravar no local storage
+function registrarUsuario(){
+    event.preventDefault();
+    var usuario = document.getElementById("usuario-input").value;
+    var email = document.getElementById("email-input").value;
+    var senha = document.getElementById("senha-input").value;
+
+    //criar classe usuario com valores recebitos
+    var classeUsuario = {
+        usuario:usuario,
+        email:email,
+        senha:senha
+    }
+
+    //transformar classe usuario para o formato json
+    var json = JSON.stringify(classeUsuario);
+    //armazenar valores no local storage
+    localStorage.setItem(usuario, json);
+
+    alert("Usuário cadastrado com sucesso!");
+    console.log(usuario);
+    
+    //deu tudo certo! voltar para o login!
+    window.location.href = "login.html"
+}
+
+function logarUsuario(){
+    event.preventDefault();
+    // pegar valores de input do login
+    var usuario = document.getElementById("usuario-input-login").value;
+    var senha = document.getElementById("senha-input-login").value;
+
+    // buscar valores do local storage
+    var classeUsuario = localStorage.getItem(usuario);
+    console.log(classeUsuario);
+
+    var data;
+
+    // verificar se usuario está cadastrado
+    // caso verdadeiro, armazenar data
+    if (classeUsuario) {
+        data = JSON.parse(classeUsuario);
+    } else {
+        alert("Usuário ou senha incorretos");
+        return;
+    }
+
+    console.log(data);
+
+    //verificar a senha e o usuario
+    // caso verdadeiro levar para a paginha index
+    if (usuario == data.usuario && senha == data.senha) {
+        alert('Login realizado com Sucesso!');
+        window.location.href = "index.html"
+    } else {
+        alert("Usuário ou senha incorretos");
+    }
+}
