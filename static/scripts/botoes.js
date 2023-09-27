f670112c445f7d5cfb94a52ef6e680c2351f2ea4
alexanderfc <lexfco@gmail.com>
2023-09-27 17:50:06 -0300

    add cstudao

---
+++ static/scripts/botoes.js
@@ -0,0 +1,9 @@
+var som = document.getElementById("som");
+
+const botao = document.getElementById ("btn") 
+botao.onmouseover = function(){
+    
+    som.pause();
+    som.currentTime = 0;
+    som.play();
+}
