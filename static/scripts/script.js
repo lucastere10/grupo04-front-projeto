2b0404f4216dc9323be1fd6a0e09d05e7b322548
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-09-27 22:32:40 -0300

    change: responsividade
    add: pattern nos inputs de login
    change: responsividade do navbar
    add: novos icones

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -12,13 +12,11 @@ document.querySelector('.heroi2-pesquisa').addEventListener('submit', function(e
     chamarQuery(2);
 });
 
-
-
-
 //pegar informações do personagem
 //definir arquivo
 function chamarQuery(int) {
-    resetWinner()
+    resetWinner();
+    resetLoser();
     event.preventDefault(); // nao limpar console log
     var id = document.querySelector(`input[name="pesquisa${int}"]`).value; // pegar o id do input
     fetch(`https://www.superheroapi.com/api.php/${api}/${id}/`) // requisitar dados da api
@@ -61,12 +59,14 @@ function compararPowerstats() {
     // Tive que colocar 
     //if (totalPowerstats[0] > 0 && totalPowerstats[1] > 0) {
         var vencedor = totalPowerstats[0] > totalPowerstats[1] ? 1 : 2;
+        var perdedor = totalPowerstats[0] < totalPowerstats[1] ? 1 : 2;
 
         //tirar o alerta porque agora os valores iniciais atrapalham
         //alert(`Player ${vencedor} venceu o duelo!`); 
 
         //adicionar classe vencedor no heroi que vencer
         document.querySelector(`.heroi${vencedor}-card`).classList.add('vencedor');
+        document.querySelector(`.heroi${perdedor}-card`).classList.add('perdedor');
     //}   
 }
 
@@ -76,32 +76,12 @@ function resetWinner() {
     document.querySelector('.heroi2-card').classList.remove('vencedor');
 }
 
+function resetLoser() {
+    document.querySelector('.heroi1-card').classList.remove('perdedor');
+    document.querySelector('.heroi2-card').classList.remove('perdedor');
+}
+
 window.onload = function() {
     chamarQuery(1);
     chamarQuery(2);
 };
-
-//pegar informações do personagem
-//definir arquivo
-// document.querySelector('.heroi1-pesquisa').addEventListener('submit', function(event) {
-//    event.preventDefault(); // nao limpar console log
-//    var id = document.querySelector('input[name="pesquisa1"]').value; // pegar o id do input
-//    fetch(`https://www.superheroapi.com/api.php/${api}/${id}/`) // requisitar dados da api
-//        .then(response => response.json()) // parse JSON
-//        .then(data => {
-//            console.log(data)
-//            // 
-//            document.querySelector('.heroi-img').src = data.image.url;
-//            document.querySelector('.heroi-nome').textContent = data.name;
-//            document.querySelector('.heroi-nome-real').textContent = data.biography['full-name']; //nao da pra passar por conta do "-"
-//            //document.querySelector('.hero-primeira-aparicao').textContent = data.biography['first-appearance']
-//            //pegar stats
-//            document.querySelector('.poder-valor').textContent = data.powerstats['power']
-//            document.querySelector('.velocidade-valor').textContent = data.powerstats['speed']
-//            document.querySelector('.combate-valor').textContent = data.powerstats['combat']
-//            document.querySelector('.inteligencia-valor').textContent = data.powerstats['intelligence']
-//            document.querySelector('.forca-valor').textContent = data.powerstats['strength']
-//            document.querySelector('.durabilidade-valor').textContent = data.powerstats['durability']
-//        })
-//        .catch(error => console.error('Error:', error));
-// });
